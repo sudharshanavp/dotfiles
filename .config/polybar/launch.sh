44403e70ce86f89aec08ef5dc059ad565ba3f30d
Sudharshana VP <sudharshanavp@outlook.com>
2020-02-04 22:43:40 +0530

    added bottom bar and modified launch script to reflect the same

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
+POLYBAR_CONFIG=$HOME/.config/polybar/config.ini
+
 # Terminate already running bar instances
 killall -q polybar
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 
-# Launch def
-polybar -c $XDG_CONFIG_HOME/polybar/config.ini def &
+# Launch bar1 and bar2
+tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar -c $POLYBAR_CONFIG top >>/tmp/polybar1.log 2>&1 &
+polybar -c $POLYBAR_CONFIG bottom >>/tmp/polybar2.log 2>&1 &
+
+
