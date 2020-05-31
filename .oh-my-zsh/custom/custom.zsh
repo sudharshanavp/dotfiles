f7ebae2edd4341baa4a9145ff8652fc201b3276f
Sudharshana VP <sudharshanavp@outlook.com>
2020-05-31 11:57:25 +0530

    added/removed flutter paths

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -1,8 +1,9 @@
-# ENVIRONMENT VARIABLES
 
+# ENVIRONMENT VARIABLES
 export PATH="$PATH:$HOME/.local/bin:$HOME/bin:/home/linden/.gem/ruby/2.7.0/bin"
 export EDITOR=nvim
 export BROWSER=firefox
+export READER=zathura
 export XDG_CONFIG_HOME=$HOME/.config
 
 # Aliases 
@@ -15,5 +16,3 @@ else
   alias v="vim"
   alias vi="vim"
 fi
-
-# Fzf settings
